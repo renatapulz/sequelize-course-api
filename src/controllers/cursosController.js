4c6bacacf0e92eddbbe5fdeeb92f37a5fc2469c3
Renata Pulz <renata.pulz@raizen.com>
2024-07-17 08:58:06 -0300

    Implementação da rota de listagem de cursos.

--- src/controllers/cursosController.js
+++ src/controllers/cursosController.js
@@ -17,6 +17,27 @@ class CursosController {
             res.status(500).json({ mensagem: 'Erro ao cadastrar um curso.' });
         }
     }
+
+    async getAll(req, res) {
+        try {
+            const dados = await Curso.findAll({
+                attributes: ['id', 'nome', 'duracao']
+            });
+
+            if (!dados) {
+                return res.status(404).json({
+                    mensagem: 'Nenhum curso encontrado'
+                });
+            }
+
+            return res.json(dados);
+        } catch (error) {
+            console.error('Erro ao listar cursos:', error);
+            return res.status(500).json({
+                mensagem: 'Não foi possível listar os cursos'
+            });
+        }
+    }
 }
 
 module.exports = new CursosController()
--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -4,6 +4,6 @@ const CursosController = require('../controllers/cursosController')
 const cursosRoutes = new Router()
 
 cursosRoutes.post('/', (req, res) => CursosController.create(req, res));
-cursosRoutes.get('/', (req, res) => CursosController.get(req, res));
+cursosRoutes.get('/', (req, res) => CursosController.getAll(req, res));
 
 module.exports = cursosRoutes
